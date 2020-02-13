0d47fb330193791342d1faef6211d163fc447b4e
Xinjiang Shao <xinjiang.shao@peapod.com>
2020-02-13 10:07:43 -0600

    Add more details for pre ahold times

--- src/App.vue
+++ src/App.vue
@@ -69,10 +69,72 @@ export default {
           ``
       },
       {
-        from: new Date(1996, 0),
-        title: "Launched its website and became one of the earliest internet start-ups",
+        from: new Date(1998, 6),
+        title: "Fullfiled 1 million orders",
         description:
-          ``
+          `Peapod initiates service in Long Island, N.Y., with Stop &amp; Shop, and in Texas
+          with Randalls and Tom Thumb
+          In July, Peapod delivers its 1 millionth order, to a customer in Chicago`
+      },
+      {
+        from: new Date(1997, 0),
+        title: "IPO",
+        description:`
+        Peapod opens the first Stop &amp; Shop wareroom in Watertown, Mass., just
+        outside Boston.
+        In June, Peapod completes a successful initial public offering listing it’s shares
+        on the NASDAQ.
+        `
+      },
+      {
+        from: new Date(1996, 0),
+        title: "Peapod reaches a customer base of 43,200",
+        description:`
+        Peapod reaches a customer base of 43,200
+        Peapod partners with Stop & Shop (a subsidiary of Royal Ahold) to offer
+        "Peapod by Stop & Shop" in the Boston metro area
+        Peapod joins the Internet, launching its own Web site: www.peapod.com
+        Inc. magazine names Peapod to the "Inc. 500" ranking of the fastest-growing
+        U.S. private companies.
+        Peapod launches with Kroger in Columbus, OH.
+        `
+      },
+      {
+        from: new Date(1995, 0),
+        title: "Launch first advertising campaign",
+        description:`
+        Peapod launches its first advertising campaign; the company gains 4,600
+        members. (Until this time, Peapod built its customer base largely through
+        word of mouth.)
+        Peapod initiates service in Columbus, Ohio with The Kroger Company.
+        `
+      },
+      {
+        from: new Date(1993, 0),
+        title: "Partner with Safeway in San Francisco",
+        description:`
+        `
+      },
+      {
+        from: new Date(1991, 0),
+        title: "Expand to Chicago",
+        description:`
+        With Evanston, IL, proving a success, Peapod expands its service to the
+        surrounding suburbs and Chicago.
+        `
+      },
+      {
+        from: new Date(1990, 0),
+        title: "Partner with Chicago-area Jewel Food Stores",
+        description:`
+        Peapod partners with Chicago-area Jewel Food Stores to fulfill orders. During
+        the early days, Andrew, Thomas and their families do the picking and
+        packing—making deliveries with their own cars.
+
+        Peapod begins test marketing to about 400 households in Evanston, Ill. The
+        company provides software and modems for customers who have to dial-in
+        directly to the Peapod shopping system.
+        `
       },
       {
         from: new Date(1989, 0),
@@ -83,7 +145,7 @@ export default {
           product marketing (Andrew was a brand manager with Proctor & Gamble and
           Kraft) and technology expertise (Thomas was the founder of a software
           company), the brothers establish Peapod as a lifestyle solution for busy
-          families
+          families.
           Peapod software ran on MS-DOS as a client server application talking to an
           SCO Unix system running on a Dell 386. Disks were sold for $49.95.`
       },
